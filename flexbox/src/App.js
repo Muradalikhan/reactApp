b7730aa57c8b89029e331447a32343c277cca677
dev-mubasshir <murad.jaddoon@gmail.com>
2022-02-09 14:32:59 +0500

    flex box
    flex box

--- flexbox/src/App.js
+++ flexbox/src/App.js
@@ -24,6 +24,15 @@ function App() {
         <div className='items item6'>Item 6</div>
         <div className='items item7'>Item 7</div>
       </div>
+      <div className='container3'>
+        <div className='items3 item31'>Item 1</div>
+        <div className='items3 item32'>Item 2</div>
+        <div className='items3 item33'>Item 3</div>
+        <div className='items3 item34'>Item 4</div>
+        <div className='items3 item35'>Item 5</div>
+        <div className='items3 item36'>Item 6</div>
+        <div className='items3 item37'>Item 7</div>
+      </div>
     </div>
   );
 }
