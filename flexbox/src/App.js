81f9fab8893c9a9f6ecc0d78665bd630a6b225cf
dev-mubasshir <94975267+dev-mubasshir@users.noreply.github.com>
2022-02-09 14:18:56 +0500

    working on flex box
    working on flex box

---
+++ flexbox/src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Flex box</h1>
+
+      <div className='container'>
+        <div className='items item1'>Item 1</div>
+        <div className='items item2'>Item 2</div>
+        <div className='items item3'>Item 3</div>
+        <div className='items item4'>Item 4</div>
+        <div className='items item5'>Item 5</div>
+        <div className='items item6'>Item 6</div>
+        <div className='items item7'>Item 7</div>
+      </div>
+      <div className='container2'>
+        <div className='items item1'>Item 1</div>
+        <div className='items item2'>Item 2</div>
+        <div className='items item3'>Item 3</div>
+        <div className='items item4'>Item 4</div>
+        <div className='items item5'>Item 5</div>
+        <div className='items item6'>Item 6</div>
+        <div className='items item7'>Item 7</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
