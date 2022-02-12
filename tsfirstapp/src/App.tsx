8f3891a3be14c0c12306e33bc03bd1498bf12dfa
dev-mubasshir <murad.jaddoon@gmail.com>
2022-02-12 12:45:49 +0500

    working on todo app design
    working on todo app design

--- tsfirstapp/src/App.tsx
+++ tsfirstapp/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ChangeEvent, useState, useEffect } from "react";
+import { FC, ChangeEvent, useState, useEffect } from "react";
 import "./App.css";
 import TodoList from "./components/TodoList";
 import { ITask } from "./Interface";
@@ -7,8 +7,6 @@ const App: FC = () => {
   const [task, setTask] = useState<string>("");
   const [deadline, setDeadline] = useState<number>(0);
   const [todolist, SetTodolist] = useState<ITask[]>([]);
-
-  useEffect(() => {}, [todolist]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     if (event.target.name === "task") {
@@ -24,17 +22,17 @@ const App: FC = () => {
       deadline: deadline,
     };
     SetTodolist([...todolist, newTask]);
-
     setTask("");
     setDeadline(0);
   };
 
-  const deleteTask = (selectedInd:number) => {
-   SetTodolist(todolist.filter((item,ind)=>ind!==selectedInd)
-   )}
+  const deleteTask = (selectedInd: number) => {
+    SetTodolist(todolist.filter((item, ind) => ind !== selectedInd));
+  };
 
   return (
     <div className="App">
+      <h1 className="title">Todo</h1>
       <div className="Header">
         <div className="Input-container">
           <input
@@ -59,18 +57,22 @@ const App: FC = () => {
         </button>
       </div>
       <div className="todoList">
-        {todolist.length > 0
-          ? todolist.map((item, ind) => {
-              return (
-                <TodoList
-                  key={ind}
-                  task={item.taskName}
-                  deadline={item.deadline}
-                  onClick={() => deleteTask(ind)}
-                />
-              );
-            })
-          : null}
+        {todolist.length > 0 ? (
+          todolist.map((item, ind) => {
+            return (
+              <TodoList
+                key={ind}
+                task={item.taskName}
+                deadline={item.deadline}
+                onClick={() => deleteTask(ind)}
+              />
+            );
+          })
+        ) : (
+          <div style={{ marginTop: 20, color: "lightgray" }}>
+            You have no task yet
+          </div>
+        )}
       </div>
     </div>
   );
