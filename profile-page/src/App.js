9c88d9cb61760d46d101d7f6b4cc5f04debc31c5
dev-mubasshir <murad.jaddoon@gmail.com>
2022-02-18 22:59:15 +0500

    photfolio start
    photfolio start

---
+++ profile-page/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Profile1 from "./screen/section1/profile1";
+import Section2 from "./screen/section2/section2";
+
+function App() {
+  return (
+    <>
+      <Profile1 />
+      <Section2 />
+    </>
+  );
+}
+
+export default App;
