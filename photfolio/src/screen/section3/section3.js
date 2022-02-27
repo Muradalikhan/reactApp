7b9bda7271bdbab73ffb66a0ba140f60cb2f53a3
Murad khan <37217077+Muradalikhan@users.noreply.github.com>
2022-02-28 01:13:36 +0500

    skills
    skills

--- photfolio/src/screen/section3/section3.js
+++ photfolio/src/screen/section3/section3.js
@@ -1,21 +1,93 @@
 import React from "react";
 import classes from "./section3.module.css";
-import { Progress } from 'react-sweet-progress';
+import { Progress } from "react-sweet-progress";
 import "react-sweet-progress/lib/style.css";
 
 export default function Section3() {
   return (
     <div className={classes.section3}>
       <div className={classes.header}>
-          <h2>Skills</h2>
-          <p>Here is what I am good at with </p>
+        <h2>Skills</h2>
+        <p>Here is what I am good at with </p>
       </div>
       <div className={classes.Content}>
         <div className={classes.left}>
-          <label>Html</label>
-        <Progress percent={88} status="success" className={classes.progressBar}/>
+          <div className={classes.leftContent}>
+            <label>Html</label>
+            <Progress
+              percent={88}
+              status="success"
+              className={classes.progressBar}
+            />
+            <label>Html</label>
+            <Progress
+              percent={80}
+              status="success"
+              className={classes.progressBar}
+            />
+            <label>Html</label>
+            <Progress
+              percent={88}
+              status="success"
+              className={classes.progressBar}
+            />
+            <label>Html</label>
+            <Progress
+              percent={80}
+              status="success"
+              className={classes.progressBar}
+            />
+            <label>Html</label>
+            <Progress
+              percent={88}
+              status="success"
+              className={classes.progressBar}
+            />
+            <label>Html</label>
+            <Progress
+              percent={88}
+              status="success"
+              className={classes.progressBar}
+            />
+          </div>
         </div>
-        <div className={classes.right}>right</div>
+        <div className={classes.right}>
+          <div className={classes.innerRight}>
+          <label>html</label>
+          <Progress
+            percent={88}
+            status="success"
+            type="circle"
+            width={200}
+            strokeWidth={7}
+          />
+          <label>html</label>
+          <Progress
+            percent={88}
+            status="success"
+            type="circle"
+            width={200}
+            strokeWidth={7}
+          />
+          <label>html</label>
+          <Progress
+            percent={88}
+            status="success"
+            type="circle"
+            width={200}
+            strokeWidth={7}
+          />
+          <label>html</label>
+          <Progress
+            percent={88}
+            status="success"
+            type="circle"
+            width={200}
+            strokeWidth={7}
+          />
+          </div>
+         
+        </div>
       </div>
     </div>
   );
