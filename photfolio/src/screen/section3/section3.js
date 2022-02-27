6709bc5e93980cc80b3a1261a9b8ff9f2a7befc4
Murad khan <37217077+Muradalikhan@users.noreply.github.com>
2022-02-27 23:55:27 +0500

    some changes
    some changes

--- photfolio/src/screen/section3/section3.js
+++ photfolio/src/screen/section3/section3.js
@@ -1,15 +1,20 @@
 import React from "react";
 import classes from "./section3.module.css";
+import { Progress } from 'react-sweet-progress';
+import "react-sweet-progress/lib/style.css";
 
 export default function Section3() {
   return (
     <div className={classes.section3}>
       <div className={classes.header}>
-          <p>header</p>
-          <p>headerasdasdddddddd</p>
+          <h2>Skills</h2>
+          <p>Here is what I am good at with </p>
       </div>
       <div className={classes.Content}>
-        <div className={classes.left}>left</div>
+        <div className={classes.left}>
+          <label>Html</label>
+        <Progress percent={88} status="success" className={classes.progressBar}/>
+        </div>
         <div className={classes.right}>right</div>
       </div>
     </div>
