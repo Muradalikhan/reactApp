3f7ec118ff91ae548e6abf35a3f9ae0dd1973303
Murad khan <37217077+Muradalikhan@users.noreply.github.com>
2022-04-03 23:43:20 +0500

    working on scroll
    working on scroll

--- photfolio/src/screen/section4/section4.js
+++ photfolio/src/screen/section4/section4.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 import ProjectCard from "../../component/ProjectCard";
 import classes from "./section4.module.css";
 
 export default function Section4() {
+  const myRef = useRef();
+
+  const doScroll = () => {
+    myRef.scrollBy(30, 0);
+  };
   return (
     <div className={classes.section4}>
       <div className={classes.header}>
@@ -12,11 +17,19 @@ export default function Section4() {
         <p>below are my best projects</p>
       </div>
       <div className={classes.Content}>
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        <div style={{ flex1: 0.1 }}>
+          <i className="fa fa-arrow-left"></i>
+        </div>
+        <div className={classes.Projects} style={{ flex1: 0.7 }} ref={myRef}>
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+        </div>
+        <div style={{ flex1: 0.1 }}>
+          <i className="fa fa-arrow-right" onClick={doScroll}></i>
+        </div>
       </div>
     </div>
   );
