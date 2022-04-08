fbe6f567a60fec79c16251abaea4ee638076224f
Murad khan <37217077+Muradalikhan@users.noreply.github.com>
2022-04-08 23:42:58 +0500

    created api
    created api

---
+++ backend/codeWithVishal/client/src/pages/Login.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBValidation,
+  MDBIcon,
+  MDBBtn,
+  MDBCardFooter,
+  MDBSpinner,
+} from "mdb-react-ui-kit";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../redux/features/authSlice";
+import { toast } from "react-toastify";
+
+const intialValue = {
+  email: "",
+  password: "",
+};
+const Login = () => {
+  const [formVal, setFormVal] = useState(intialValue);
+  const { email, password } = formVal;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setFormVal({ ...formVal, [name]: value });
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (email && password) {
+      dispatch(login({ formVal, toast, navigate }));
+    }
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: "50px",
+      }}
+    >
+      <MDBCard alignment="center">
+        <MDBIcon fas icon="user-circle" className="fa-2x" />
+        <h4>Sign In</h4>
+        <MDBCardBody>
+          <MDBValidation
+            onSubmit={submitHandler}
+            noValidate
+            className="row g-3"
+          >
+            <div className="col-md-12">
+              <MDBInput
+                label="Email"
+                type="email"
+                name="email"
+                value={email}
+                required
+                invalid
+                autoComplete="username"
+                validation="please enter valid email"
+                onChange={changeHandler}
+                size="lg"
+              />
+            </div>
+            <div className="col-md-12">
+              <MDBInput
+                label="Password"
+                type="password"
+                name="password"
+                value={password}
+                required
+                invalid
+                autoComplete="current-password"
+                validation="please enter valid password"
+                onChange={changeHandler}
+                size="lg"
+              />
+            </div>
+            <div className="col-md-12">
+              <MDBBtn style={{ width: "100%" }} className="mt-2" size="lg">
+                Login
+              </MDBBtn>
+            </div>
+          </MDBValidation>
+          <MDBCardFooter>
+            <Link to="/register">Don't have account? Signup</Link>
+          </MDBCardFooter>
+        </MDBCardBody>
+      </MDBCard>
+    </div>
+  );
+};
+
+export default Login;
---
+++ backend/codeWithVishal/client/src/redux/features/authSlice.js
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import * as api from "../api";
+
+export const login = createAsyncThunk(
+  "auth/login",
+  async ({ formVal, toast, navigate }) => {
+    try {
+      const response = await api.signIn(formVal);
+      toast.success("Login successfully");
+      navigate("/");
+      return response.data;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+
+const AuthSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: null,
+    error: "",
+    loading: false,
+  },
+  extraReducers: {
+    [login.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [login.fulfilled]: (state, action) => {
+      state.loading = false;
+      localStorage.setItem("profile", JSON.stringify({ ...action.payload }));
+      state.user = action.payload;
+    },
+    [login.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload.message;
+    },
+  },
+});
+
+export default AuthSlice.reducer;
