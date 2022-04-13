0b28f1dbdc843bc53090becbf186b21ef305a803
Murad khan <37217077+Muradalikhan@users.noreply.github.com>
2022-04-13 21:24:16 +0500

    created middle ware
    created middle ware

--- backend/codeWithVishal/client/src/redux/store.js
+++ backend/codeWithVishal/client/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthReducer from "./features/authSlice.js";
+import TourReducer from "./features/tourSlice.js";
 
 const store = configureStore({
   reducer: {
     auth: AuthReducer,
+    tour: TourReducer,
   },
 });
 
