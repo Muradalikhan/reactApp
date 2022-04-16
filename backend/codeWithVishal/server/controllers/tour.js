b43670d4c8a8587f5e7747c4c155e91f33285106
Murad khan <37217077+Muradalikhan@users.noreply.github.com>
2022-04-16 23:44:11 +0500

    tour by Id controlled created
    tour by Id controlled created

--- backend/codeWithVishal/server/controllers/tour.js
+++ backend/codeWithVishal/server/controllers/tour.js
@@ -18,8 +18,17 @@ export const createTour = async (req, res) => {
 export const getTours = async (req, res) => {
   try {
     const tours = await TourModal.find();
-    console.log('tours=>',tours)
     res.status(200).json(tours);
+  } catch (err) {
+    res.status(404).json({ message: "something went wrong" });
+  }
+};
+
+export const getTour = async (req, res) => {
+  const { id } = req.param;
+  try {
+    const tour = await TourModal.findById(id);
+    res.status(200).json(tour);
   } catch (err) {
     res.status(404).json({ message: "something went wrong" });
   }
