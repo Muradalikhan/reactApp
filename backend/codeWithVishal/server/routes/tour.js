084ffd978ec7ddad95f880d9b8d34b970437e78c
Murad khan <37217077+Muradalikhan@users.noreply.github.com>
2022-04-16 23:51:27 +0500

    route created

--- backend/codeWithVishal/server/routes/tour.js
+++ backend/codeWithVishal/server/routes/tour.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
 
-import { createTour, getTours } from "../controllers/tour.js";
+import { createTour, getTour, getTours } from "../controllers/tour.js";
 import auth from "../middleware/auth.js";
 
 router.post("/tour", auth, createTour);
 router.get("/tour", getTours);
+router.get("/tour/:id", getTour);
 
 export default router;
