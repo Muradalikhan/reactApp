762c210ff1891bd81644aad982186eda946f9a73
Murad khan <37217077+Muradalikhan@users.noreply.github.com>
2022-04-26 00:53:33 +0500

    Update tourcard

--- backend/codeWithVishal/client/src/components/TourCard/TourCard.js
+++ backend/codeWithVishal/client/src/components/TourCard/TourCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   MDBCard,
   MDBCardBody,
@@ -10,12 +10,29 @@ import {
 } from "mdb-react-ui-kit";
 import "./TourCard.css";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 
 export const TourCard = ({ title, name, description, imageFile, _id }) => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const [writeAccess, setWriteAccess] = useState(false);
+
+  useEffect(() => {
+    console.log(location.pathname);
+    if (location.pathname === "/dashboard") {
+      setWriteAccess(true);
+    } else {
+      setWriteAccess(false);
+    }
+  }, []);
   const goToDetail = (id) => {
     navigate(`/singleTour/${id}`);
   };
+  const goToEdit = (id) => {
+    navigate(`/editTour/${id}`);
+  };
+  const goToDelete = (id) => {};
   return (
     <MDBCard style={{ maxWidth: "22rem", margin: "20px", padding: "15px" }}>
       <MDBRipple
@@ -45,6 +62,16 @@ export const TourCard = ({ title, name, description, imageFile, _id }) => {
             : description}
         </MDBCardText>
         <MDBBtn onClick={() => goToDetail(_id)}>View Details</MDBBtn>
+        {writeAccess && (
+          <div>
+            <MDBBtn className="m-2" onClick={() => goToEdit(_id)}>
+              Edit
+            </MDBBtn>
+            <MDBBtn className="m-2" onClick={() => goToDelete(_id)}>
+              Delete
+            </MDBBtn>
+          </div>
+        )}
       </MDBCardBody>
     </MDBCard>
   );
