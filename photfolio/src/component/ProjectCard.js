35aaa4625ad3fb3e0a4decdd28aaee8e7f8fb840
Murad khan <37217077+Muradalikhan@users.noreply.github.com>
2022-06-11 19:44:58 +0500

    updated project card with real data

--- photfolio/src/component/ProjectCard.js
+++ photfolio/src/component/ProjectCard.js
@@ -2,13 +2,23 @@ import React from "react";
 import "./ProjectCard.css";
 import img from "../images/project.jpg";
 
-const ProjectCard = () => {
+const ProjectCard = ({ item }) => {
+  const { img, title, demoLink, sourceLink, technologies } = item;
   return (
     <div className="project-card">
       <img src={img} alt="img" className="project-img" />
-      <h3 className="project-title">Project Title</h3>
-      <a className="project-btn">Demo</a>
-      <a className="project-btn">Source</a>
+      <h3 className="project-title">{title}</h3>
+      <a className="project-btn" href={demoLink} target="_blank">
+        Demo
+      </a>
+      <a className="project-btn" href="#">
+        Source
+      </a>
+      <div className="project-technology">
+        {technologies?.map((item, index) => (
+          <img src={item} alt="img" />
+        ))}
+      </div>
     </div>
   );
 };
